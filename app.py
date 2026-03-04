cba8741720c3fe491aaf6019cc49317029576a10
Thoufiqz-26 <23f41a3351@kec.ac.in>
2026-03-04 23:45:23 +0530

    Improved UI design

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import PyPDF2
 
 app = Flask(__name__)
 
+def extract_text_from_pdf(file):
+    reader = PyPDF2.PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
 @app.route("/")
 def home():
-    return "Resume Analyzer is Live!"
+    return render_template("index.html")
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    file = request.files["resume"]
+    text = extract_text_from_pdf(file)
+
+    word_count = len(text.split())
+
+    return render_template("result.html", words=word_count)
 
 if __name__ == "__main__":
     app.run()
